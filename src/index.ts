b3522126e2885694494197bc0297c6268b66fbc8
mvtcode <macvantan@gmail.com>
2026-01-08 13:36:03 +0700

    Done for test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,43 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import mqtt from "mqtt";
 
-console.log('Hello, TypeScript!');
-console.log('Environments', process.env);
+const MQTT_SERVER = process.env.MQTT_SERVER!;
+
+const clientPub = mqtt.connect(MQTT_SERVER, {
+  clientId: "tanmvPub",
+  username: process.env.MQTT_USERNAME_PUB,
+  password: process.env.MQTT_PASSWORD_PUB,
+});
+
+const clientSub = mqtt.connect(MQTT_SERVER, {
+  clientId: "tanmvSub",
+  username: process.env.MQTT_USERNAME_SUB,
+  password: process.env.MQTT_PASSWORD_SUB,
+});
+
+clientPub.on("connect", () => {
+  console.log("clientPub connected!");
+  setInterval(() => {
+    clientPub.publish("test/topic", `Hello from FreeMQTT! ${Date.now()}`, {
+      qos: 1,
+      retain: false,
+    });
+  }, 1000);
+});
+
+clientSub.on("connect", () => {
+  console.log("clientSub connected!");
+  clientSub.subscribe("test/topic");
+  clientSub.on("message", (topic, message) => {
+    console.log("clientSub received message", topic, message.toString());
+  });
+});
+
+/*
+OUT:
+clientSub connected!
+clientPub connected!
+clientSub received message test/topic Hello from FreeMQTT! 1767853157178
+clientSub received message test/topic Hello from FreeMQTT! 1767853158178
+*/
